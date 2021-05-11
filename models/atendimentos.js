f592fccf12728a221a540079fda1eb32b6935f4b
MatioZG <matheus.fongaro@gmail.com>
2021-05-11 08:40:45 -0400

    commit on 'main'

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,51 +2,49 @@ const moment = require('moment')
 const axios = require('axios')
 const conexao = require('../infraestrutura/databse/conexao')
 const repo = require('../repositories/atendimentos')
-const { query } = require('../infraestrutura/databse/conexao')
 
 class Atendimento {
-    constructor(){
-        this.dataEhValida = ({data, dataCriacao})=> 
+    constructor() {
+        this.dataEhValida = ({ data, dataCriacao }) =>
             moment(data).isSameOrAfter(dataCriacao)
-        this.clienteEhValido = tamanho => tamanho >=5
+        this.clienteEhValido = ({tamanho}) => tamanho >= 5
 
-        this.valida = params =>
-            this.validacoes.filter(campo =>{
+        this.valida = parametros =>
+            this.validacoes.filter(campo => {
                 const { nome } = campo
-                const param = params[nome] 
-                return !campo.valido(param)
+                const parametro = parametros[nome]
+                
+                return !campo.valido(parametro)
             })
 
-            this.validacoes = [
-                {
-                    nome: 'data',
-                    valido: this.dataEhValida,
-                    mensagem: 'Data deve ser maior ou igual a data atual'
-                },
-                {
-                    nome: 'cliente',
-                    valido: this.clienteEhValido,
-                    mensagem: 'Cliente deve ter pelo menos cinco caracteres'
-                }
-            ]
+        this.validacoes = [
+            {
+                nome: 'data',
+                valido: this.dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: this.clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
+            }
+        ]
     }
 
     adiciona(atendimento) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
 
-        const params = {
-                data: { data, dataCriacao },
-                cliente: { tamanho: atendimento.cliente.length }
+        const parametros = {
+            data: { data, dataCriacao },
+            cliente: { tamanho: atendimento.cliente.length }
         }
 
-        const erros = this.valida(params)
+        const erros = this.valida(parametros)
         const existemErros = erros.length
 
         if (existemErros) {
-            return new Promise((resolve, reject)=>{
-                reject(erros)
-            })
+            return new Promise((resolve, reject)=> reject(erros))
         } else {
             const atendimentoDatado = { ...atendimento, dataCriacao, data }
 
